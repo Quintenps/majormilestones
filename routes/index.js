e345522a80fed29aa5a56d6efd522c1031b19388
Quinten Peels <quinten@peels.me>
2022-12-20 14:26:49 +0100

    feature - onlyonce buttons form fill + contact page

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,10 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Lean-Agile transformation services' });
 });
 
+router.get('/contact', function(req, res, next) {
+  res.render('contact', { title: 'Contact information' });
+});
+
 router.get('/register', function(req, res, next) {
   res.render('register', { title: 'Course registration' });
 });
